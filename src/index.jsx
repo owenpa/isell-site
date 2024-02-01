15072424cff1ec0b7104334b8c308b4ce06a26e1
owen <on-hill@outlook.com>
2024-02-01 13:41:17 -0600

    make eslint happy

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+import sendToVercelAnalytics from './vitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
