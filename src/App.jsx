8cc8552dd8ee1c1f1f66ad0b2bcc6e5b931dd7b2
owen <on-hill@outlook.com>
2024-02-01 13:40:34 -0600

    remove vercel speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import './main.css';
 import './Navbar';
-// eslint-disable-next-line import/no-unresolved
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 function App() {
   return (
-    <div className="App">
-      <SpeedInsights />
-    </div>
+    <div className="App" />
   );
 }
 
