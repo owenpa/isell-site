bdd36486a464adee52673eaaefb2623967d20722
owen <on-hill@outlook.com>
2024-02-01 15:02:43 -0600

    scss

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './assets/main.scss';
 import reportWebVitals from './reportWebVitals';
 import sendToVercelAnalytics from './vitals';
 
