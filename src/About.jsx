7ddb3530f33070111bc6cf1eacb565a0dfa073e7
Owen Hill <on-hill@outlook.com>
2024-02-02 17:02:48 -0600

    trim extra space

--- src/About.jsx
+++ src/About.jsx
@@ -30,7 +30,6 @@ function About() {
         iure reprehenderit qui in ea voluptate velit esse quam nihil
         molestiae consequatur, vel illum qui dolorem eum fugiat quo
         voluptas nulla pariatur?
-
       </p>
     </div>
   );
