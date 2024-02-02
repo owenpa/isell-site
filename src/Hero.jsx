4ad78c6ed056622114462a71cd467422e95a5930
Owen Hill <on-hill@outlook.com>
2024-02-02 15:40:42 -0600

    add fake star rating visual

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -11,7 +11,10 @@ function Hero() {
         <div id="hero-app-download">
           <img src={appstoreBadge} id="appstore-badge" alt="Apple Store call-to-action" />
           <img src={googleBadge} id="google-badge" alt="Google Play Store call-to-action" />
+          <p className="app-rating">★★★★★</p>
+          <p className="app-rating">★★★★☆</p>
         </div>
+
         <p className="faint-text">(API requests to retrieve and display star ratings)</p>
       </div>
       <img src={angledPhone} id="angled-phone" alt="Angled iPhone displaying nothing" />
