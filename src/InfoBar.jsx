1c5244ec4185fdedf20efd968283f256d0129955
owen <on-hill@outlook.com>
2024-02-01 18:23:16 -0600

    create base info bar

---
+++ src/InfoBar.jsx
@@ -0,0 +1,26 @@
+function InfoBar() {
+  return (
+    <div id="info-bar-container">
+      <ul>
+        <li>
+          <p className="info-stat">1k</p>
+          <p>Active users</p>
+        </li>
+        <li>
+          <p className="info-stat">2k</p>
+          <p>Total downloads</p>
+        </li>
+        <li>
+          <p className="info-stat">3k</p>
+          <p>Customers</p>
+        </li>
+        <li>
+          <p className="info-stat">1</p>
+          <p>Supporters</p>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default InfoBar;
