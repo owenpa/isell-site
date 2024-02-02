b3aafd490afab07b74ee398376956146f1b45265
Owen Hill <on-hill@outlook.com>
2024-02-02 15:41:17 -0600

    style features list

--- src/Features.jsx
+++ src/Features.jsx
@@ -8,22 +8,22 @@ function Features() {
       </div>
       <div id="grid-right">
         <p className="container-title">FEATURES</p>
-        <h2>Prominent feature A</h2>
+        <h2>Prominent feature list A</h2>
         <p className="faint-description">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
         <div id="features-list">
-          <p>1</p>
+          <button type="button">1</button>
           <div id="features-list-item">
-            <p>Feature title</p>
-            <p className="faint-description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+            <p className="feature-title">Feature title</p>
+            <p className="faint-description">Lorem ipsum dolor sit amet, adipiscing elit.</p>
           </div>
-          <p>2</p>
+          <button type="button">2</button>
           <div id="features-list-item">
-            <p>Feature title</p>
-            <p className="faint-description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+            <p className="feature-title">Feature title</p>
+            <p className="faint-description">Lorem ipsum dolor sit amet, consectetuer elit.</p>
           </div>
-          <p>3</p>
+          <button type="button">3</button>
           <div id="features-list-item">
-            <p>Feature title</p>
+            <p className="feature-title">Feature title</p>
             <p className="faint-description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
           </div>
         </div>
