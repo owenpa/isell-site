2e726f026a7c6c82be508dc64643feb99ceec9a9
Owen Hill <on-hill@outlook.com>
2024-02-02 15:40:15 -0600

    rearrange developer section

--- src/Developer.jsx
+++ src/Developer.jsx
@@ -12,16 +12,16 @@ function Developer() {
     <div id="developer-container">
       <h1>Brought to you by Owen Hill</h1>
       <img src={me} alt="man smiling" id="self-portrait" />
+      <div id="socials-container">
+        <a href="https://github.com/owenpa" rel="noreferrer" target="_blank"><img src={githubLogo} alt="github logo" className="tech-logo" /></a>
+        <a href="https://www.linkedin.com/in/owenghill/" rel="noreferrer" target="_blank"><img src={linkedinLogo} alt="linkedin logo" className="tech-logo" /></a>
+      </div>
       <div id="tech-stack-container">
         <img src={htmlLogo} alt="html logo" className="tech-logo" />
         <img src={jsLogo} alt="javascript logo" className="tech-logo" />
         <img src={reactLogo} alt="react logo" className="tech-logo" />
         <img src={sassLogo} alt="sass logo" className="tech-logo" />
         <img src={vsLogo} alt="vscode logo" className="tech-logo" />
-      </div>
-      <div id="tech-stack-container">
-        <img src={githubLogo} alt="github logo" className="tech-logo" />
-        <img src={linkedinLogo} alt="linkedin logo" className="tech-logo" />
       </div>
     </div>
   );
