482efcc39618e69dbe205965a08cb20a04b68733
Owen Hill <on-hill@outlook.com>
2024-02-02 15:41:01 -0600

    add links to navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,11 +3,11 @@ function Navbar() {
     <div id="navbar-container">
       <ul>
         <h3>iSell Demo</h3>
-        <li>Home</li>
-        <li>Features</li>
-        <li>Pricing</li>
-        <li>About</li>
-        <li>Developer</li>
+        <li><a href="#hero-container">Home</a></li>
+        <li><a href="#features-container">Features</a></li>
+        <li><a href="#pricing-container">Pricing</a></li>
+        <li><a href="#about-container">About</a></li>
+        <li><a href="#developer-container">Developer</a></li>
       </ul>
     </div>
   );
